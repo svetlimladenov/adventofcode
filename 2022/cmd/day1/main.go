ac15b0761b42b71f8ae9d2dfab50db629ba62f4b
svetlimladenov <svetlimladenov@gmail.com>
2022-12-02 00:37:44 +0200

    Solve part one on day1

--- 2022/cmd/day1/main.go
+++ 2022/cmd/day1/main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
-
-	goutils "github.com/svetlimladenov/adventofcode/go_utils/pkg"
+	"strconv"
+	"strings"
 )
 
+//go:embed input.txt
+var input string
+
 func main() {
-	res := goutils.ParseInt("2022")
-	fmt.Println(res)
+	elfs := strings.Split(input, "\n\n")
+
+	max := 0
+	for _, elf := range elfs {
+		cals := sum(elf)
+		if cals > max {
+			max = cals
+		}
+	}
+
+	fmt.Println(max)
+}
+
+func sum(elf string) int {
+	sum := 0
+	for _, cal := range strings.Split(elf, "\n") {
+		parsedCal, _ := strconv.Atoi(cal)
+		sum += parsedCal
+	}
+	return sum
 }

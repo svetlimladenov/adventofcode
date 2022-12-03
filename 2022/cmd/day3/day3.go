20a6f0cc41acc4d9861ef93b38aacb5c051db9f6
svetlimladenov <svetlimladenov@gmail.com>
2022-12-03 19:11:12 +0200

    Add solution for the second part of day3

--- 2022/cmd/day3/day3.go
+++ 2022/cmd/day3/day3.go
@@ -12,19 +12,38 @@ var input string
 func main() {
 	rucksacks := strings.Split(input, "\n")
 
+	secondPart(rucksacks)
+}
+
+func secondPart(rucksacks []string) {
+	result := 0
+
+	for i := 0; i < len(rucksacks); i += 3 {
+	group:
+		for firstElfIndex := 0; firstElfIndex < len(rucksacks[i]); firstElfIndex++ {
+			for secondElfIndex := 0; secondElfIndex < len(rucksacks[i+1]); secondElfIndex++ {
+				if rucksacks[i][firstElfIndex] == rucksacks[i+1][secondElfIndex] {
+					for thirdElfIndex := 0; thirdElfIndex < len(rucksacks[i+2]); thirdElfIndex++ {
+						if rucksacks[i][firstElfIndex] == rucksacks[i+2][thirdElfIndex] {
+							result += calculateTypeScore(int(rucksacks[i][firstElfIndex]))
+							break group
+						}
+					}
+				}
+			}
+		}
+	}
+	fmt.Println(result)
+}
+
+func firstPart(rucksacks []string) {
 	score := 0
 	for _, sack := range rucksacks {
 	sackLoop:
 		for i := 0; i < len(sack)/2; i++ {
 			for j := len(sack) / 2; j < len(sack); j++ {
 				if sack[i] == sack[j] {
-					if sack[i] > 'Z' {
-						fmt.Println(string(sack[i]), int(sack[i]-96))
-						score += int(sack[i]) - 96
-					} else {
-						fmt.Println(string(sack[i]), int(sack[i]-38))
-						score += int(sack[i]) - 38
-					}
+					score += calculateTypeScore(int(sack[i]))
 					break sackLoop
 				}
 			}
@@ -32,4 +51,12 @@ func main() {
 	}
 
 	fmt.Println(score)
+}
+
+func calculateTypeScore(itemType int) int {
+	if itemType > 'Z' {
+		return int(itemType) - 96
+	} else {
+		return int(itemType) - 38
+	}
 }

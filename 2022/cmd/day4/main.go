7b85951c9e727de0ae769631512e94c3946f9f22
svetlimladenov <svetlimladenov@gmail.com>
2022-12-05 23:41:21 +0200

    Add day 4 second part

--- 2022/cmd/day4/main.go
+++ 2022/cmd/day4/main.go
@@ -23,17 +23,32 @@ func main() {
 		secondAssigmentStart, _ := strconv.Atoi(secondAssigmentRange[0])
 		secondAssigmentEnd, _ := strconv.Atoi(secondAssigmentRange[1])
 
-		if firstAssigmentStart >= secondAssigmentStart && firstAssigmentEnd <= secondAssigmentEnd {
+		// .23......  2-3
+		// ...45....  4-5
+
+		// .....678.. 6-8
+		// 123....... 1-3
+		if secondAssigmentStart > firstAssigmentEnd {
 			fmt.Println(assignments)
-			counter++
 			continue
 		}
 
-		if secondAssigmentStart >= firstAssigmentStart && secondAssigmentEnd <= firstAssigmentEnd {
-			counter++
+		if secondAssigmentEnd < firstAssigmentStart {
 			fmt.Println(assignments)
 			continue
 		}
+		counter++
 	}
 	fmt.Println(counter)
+}
+
+func isTotalOverlap(firstAssigmentStart, firstAssigmentEnd, secondAssigmentStart, secondAssigmentEnd int) bool {
+	if firstAssigmentStart >= secondAssigmentStart || firstAssigmentEnd <= secondAssigmentEnd {
+		return true
+	}
+
+	if secondAssigmentStart >= firstAssigmentStart || secondAssigmentEnd <= firstAssigmentEnd {
+		return true
+	}
+	return false
 }

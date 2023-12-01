298abea2c3c10a68ef3f20f8c6d9d142eac8b4b9
svetlimladenov <svetlimladenov@gmail.com>
2023-12-02 01:03:19 +0200

    Add solutions for 2023 with swift

---
+++ 2020/day1/Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "day1",
+    dependencies: [],
+    targets: [
+          .executableTarget(
+            name: "day1",
+            dependencies: [],
+            path: "Sources"
+            ),
+    ]
+)
---
+++ 2020/day3/Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "day3",
+    dependencies: [],
+    targets: [
+          .executableTarget(
+            name: "day3",
+            dependencies: [],
+            path: "Sources"
+            ),
+    ]
+)

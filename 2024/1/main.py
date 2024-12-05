8dc4c611e5ab8ce8897cec97878e4823199ed06f
svetlimladenov <svetlimladenov@gmail.com>
2024-12-05 10:29:11 +0200

    Use snake case

--- 2024/1/main.py
+++ 2024/1/main.py
@@ -4,17 +4,17 @@ def main():
         input = file.read().rstrip()
 
     data = input.split()
-    firstColumn, secondColumn = sorted(data[0::2]), sorted(data[1::2])
+    first_column, second_column = sorted(data[0::2]), sorted(data[1::2])
 
-    print(f"First: {partOne(firstColumn, secondColumn)}")
-    print(f"Second: {partTwo(firstColumn, secondColumn)}")
+    print(f"First: {partOne(first_column, second_column)}")
+    print(f"Second: {partTwo(first_column, second_column)}")
 
 
-def partOne(firstColumn: list, secondColumn: list) -> int:
+def partOne(first_col: list, second_col: list) -> int:
     res = 0
 
-    for i, num in enumerate(firstColumn):
-        res += abs(int(num) - int(secondColumn[i]))
+    for i, num in enumerate(first_col):
+        res += abs(int(num) - int(second_col[i]))
 
     return res
 
@@ -23,8 +23,8 @@ def partTwo(first: list, second: list) -> int:
     res = 0
     for num in first:
         count = 0
-        for secondNum in second:
-            if num == secondNum:
+        for second_num in second:
+            if num == second_num:
                 count += 1
         res += count * int(num)
 
